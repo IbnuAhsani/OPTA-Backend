c44ae232989399db60cbe94c73cccb9bdbf3d059
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-11-03 00:17:09 +0700

    Bus v.0.1.0: create Bus table

---
+++ database/migrations/2018_11_02_162843_create_bus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bus_number');
+            $table->bigInteger('price');
+            $table->integer('bus_admin_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus');
+    }
+}
