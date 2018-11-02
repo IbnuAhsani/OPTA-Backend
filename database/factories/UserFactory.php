185d56657e927200ee9aafb3afb09b4f7e3224e2
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-11-02 23:23:29 +0700

    User v.0.4.0: create DB seeder for User table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => bcrypt('secret'), // secret
+        'name' => $faker->name,
+        'address' => $faker->address,
+        'balance' => rand(10000, 50000),
+        'role' => rand(0, 1),
         'remember_token' => str_random(10),
     ];
 });
