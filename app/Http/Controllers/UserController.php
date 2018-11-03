c6389eb80902db621420eb50478205f6e779169a
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-11-03 10:29:19 +0700

    User v.0.5.0: add pay function to controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Bus;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,5 +12,32 @@ class UserController extends Controller
         $user = User::find($user_id);
 
         return response()->json($user, 200);
+    }
+
+    public function pay(Request $request){
+        $user_id = $request->input('user_id');
+        $bus_id = $request->input('bus_id');
+
+        $user = User::find($user_id);
+        $bus = Bus::find($bus_id); 
+
+        if($user['balance'] < $bus['price']) {
+            return response()->json([
+                "status_code" => 403,
+                "error" => "Saldo Kamu tidak cukup untuk membayar tiket Bus ini",
+                "price" => $bus['price']
+            ]);
+        } else {
+            $newBalance = $user['balance'] - $bus['price'];
+    
+            $user->balance = $newBalance;
+            $user->save();
+    
+            return response()->json([
+                "status_code" => 200,
+                "error" => null,
+                "price" => $bus['price'], 
+            ]);
+        }
     }
 }
