7c159aac65467568d5582754e274e076770d3f3c
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-11-03 23:19:21 +0700

    Bus Admin v.0.1.0: create Bus Admin table

---
+++ database/migrations/2018_11_03_153152_create_bus_admin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus_admin', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 50);
+            $table->string('password', 255);
+            $table->string('company_name', 100);
+            $table->bigInteger('balance')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus_admin');
+    }
+}
