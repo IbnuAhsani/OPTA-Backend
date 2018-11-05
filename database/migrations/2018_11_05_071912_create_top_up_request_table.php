be601c16c987bdda7dc958df2866553d63961ca1
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-11-05 16:07:56 +0700

    User v.0.9.1: create TopUpRequest table

---
+++ database/migrations/2018_11_05_071912_create_top_up_request_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopUpRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('top_up_request', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('accepted_status')->default(0);
+            $table->integer('unique_code')->unique();
+            $table->integer('nominal');
+            $table->bigInteger('request_time');
+            $table->bigInteger('expire_time');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('top_up_request');
+    }
+}
