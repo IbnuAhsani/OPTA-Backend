d015ac513a86ee2ca228b722ff7c6cc3d50eaea4
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-11-20 16:03:37 +0700

    Create Withdraw Request table

---
+++ database/migrations/2018_11_20_082144_create_withdraw_request_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWithdrawRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('withdraw_request', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('nominal');
+            $table->boolean('accepted_status');
+            $table->integer('bus_admin_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('withdraw_request');
+    }
+}
