1f5e33283927f6bc7303f66ffd731a77a8e31633
miun173 <fahmifan173@gmail.com>
2018-11-26 05:41:12 +0700

    improve styling

--- resources/views/maskapai/empty_dashboard.blade.php
+++ resources/views/maskapai/empty_dashboard.blade.php
@@ -1,5 +1,25 @@
 @extends('maskapai.template')
 
+@section('style')
+<style>
+  a {
+    text-decoration: unset;
+    color: #607d8b;
+  }
+
+  .input_add {
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    padding: 4px;
+  }
+
+  #add:hover {
+    color: #2196f3;
+  }
+
+</style>
+@endsection
+
 <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
 @include("maskapai.header")
 @include("maskapai.sidebar")
@@ -11,7 +31,7 @@
             <div style="margin: auto" class="mdl-cell--12-col">
               <h4 style="text-align: center"><i>Belum ada bus</i></h4>
             </div>
-            <table style="width: 70%; margin: auto; overflow: auto" class="mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+            <table style="width: 70%; margin: auto" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
               <thead>
                 <tr>
                   <th style="text-align: left;">No. Bus</th>
@@ -20,17 +40,21 @@
                 </tr>
               </thead>
               <tbody>
-                <tr style="background: #ccc">
+                <tr style="background: #f5f5f5">
                   <form id="add_bus" method="POST" action="/maskapai/add_bus">
                     {{ csrf_field() }}
                     <td style="text-align: left">
-                      <input style="width: 50px" type="number" name="bus_number" id="bus_number">
+                      <input class="input_add" style="width: 50px" type="number" name="bus_number" id="bus_number">
                     </td>
                     <td style="text-align: left">
-                      <small>Rp</small> <input type="number" name="price" id="price">
+                      <small>Rp</small> <input class="input_add" type="number" name="price" id="price">
                     </td>
                     <td style="text-align: center">
-                      <button id="btn_add">+</button>
+                      <a>
+                        <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="add">
+                          <i class="material-icons">add</i>
+                        </button>
+                      </a>
                     </td>
                   </form>
                 </tr>
