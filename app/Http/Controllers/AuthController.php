c93b60e6804da09920de68cb5a70965fe08211f3
miun173 <fahmifan173@gmail.com>
2018-11-26 11:32:19 +0700

    remove too nested if else by implement early return

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,42 +22,47 @@ class AuthController extends Controller
         $user = User::where('email', $email)->first();
         $bus_admin = BusAdmin::where('email', $email)->first();
 
+        // check if it's user or bus_admin
         if($user == NULL && $bus_admin == NULL){
             return response()->json([
                 'error' => 'Email tersebut tidak terdaftar pada aplikasi ini'
             ], 403);
-        } else {
-            if($user == NULL){
-                if(!app('hash')->check($password, $bus_admin->password)) {
-                    return response()->json([
-                        'error' => 'Email atau katasandi salah, mohon dicoba lagi'
-                    ], 403);
-                }
+        } 
 
-                // save user id in session  
-                $req->session()->put("user", ['id' => $bus_admin->id]);
-
-                return redirect()->route('dashboard');
-            } else {
-                if(!app('hash')->check($req['password'], $user->password)){
-                    return response()->json([
-                        'error' => 'Katasandi untuk Email tersebut salah, mohon dicoba lagi'
-                    ], 403);
-                } else {
-                    if ($user->role != 1) {
-                        // save admin id in session  
-                        $req->session()->put("admin", ['id' => $user->id]);
-
-                        return redirect()->route('top_up');
-                    } else {
-                        return response()->json([
-                            'user_id' => $user->id,
-                            'token' => $user->remember_token,
-                            'privilege' => '2'
-                        ], 200);
-                    }
-                }
+        // if it's admin
+        if($user == NULL){
+            if(!app('hash')->check($password, $bus_admin->password)) {
+                return response()->json([
+                    'error' => 'Email atau katasandi salah, mohon dicoba lagi'
+                ], 403);
             }
-        }
+
+            // save user id in session  
+            $req->session()->put("user", ['id' => $bus_admin->id]);
+
+            return redirect()->route('dashboard');
+        } 
+
+        // if it's user, but the creds not match any
+        if(!app('hash')->check($req['password'], $user->password)){
+            return response()->json([
+                'error' => 'Katasandi untuk Email tersebut salah, mohon dicoba lagi'
+            ], 403);
+        } 
+        
+        // this must be the admin
+        if ($user->role != 1) {
+            // save admin id in session  
+            $req->session()->put("admin", ['id' => $user->id]);
+
+            return redirect()->route('top_up');
+        } 
+
+        // this must be user
+        return response()->json([
+            'user_id' => $user->id,
+            'token' => $user->remember_token,
+            'privilege' => '2'
+        ], 200);
     }
 }
