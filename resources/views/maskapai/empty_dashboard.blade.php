d1e7cc7f33e7b04983b473801bf60f40ebd9621e
miun173 <fahmifan173@gmail.com>
2018-11-26 18:51:29 +0700

    change to use new navbar; inform a message to add new Bus

--- resources/views/maskapai/empty_dashboard.blade.php
+++ resources/views/maskapai/empty_dashboard.blade.php
@@ -21,7 +21,7 @@
 @endsection
 
 <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-@include("maskapai.header")
+@include("maskapai.navbar")
 @include("maskapai.sidebar")
 @section('main')
     <main class="mdl-layout__content mdl-color--grey-100">
@@ -29,7 +29,7 @@
         <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
             <!-- empty icons -->
             <div style="margin: auto" class="mdl-cell--12-col">
-              <h4 style="text-align: center"><i>Belum ada bus</i></h4>
+              <h4 style="text-align: center"><i>Tambah Bus</i></h4>
             </div>
             <table style="width: 70%; margin: auto" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
               <thead>
